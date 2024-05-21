9bdda26806e3a698cdd38c76753799fbcee75b4f
Sivasish Praharaj <sivasish48@gmail.com>
2024-05-21 20:43:33 +0530

    debugging

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 
+app.use(express.json())
 const{connectionToMongoDb} = require("./connect")
 
 const urlRoute =require ("./routes/urlRouter")
@@ -14,9 +15,12 @@ const PORT = process.env._port
 
 
 connectionToMongoDb(process.env._MONGO_URL)
+.then(()=>{
+    console.log(`Mongo Db connected`);
+})
 
 
 app.use("/url",urlRoute)
 app.listen(PORT,()=>{
     console.log(`Server is running at ${PORT}`); 
-}) 
+})  
