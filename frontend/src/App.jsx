6c863f5e1f9650c2b09a0df350bb9eaa7d2cd7d0
Sivasish Praharaj <sivasish48@gmail.com>
2024-05-25 23:56:55 +0530

    add components to the ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import SearchInput from "./Pages/SearchInput";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+        <SearchInput />
+      </div>
+    </>
+  );
+}
+
+export default App;
